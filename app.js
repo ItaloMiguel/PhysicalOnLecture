41b5fa46d01e2862817f6bae06c98ed627dea2dd
ItaloMiguel <italomiguelleite16@gmail.com>
2024-05-25 00:30:51 -0300

    Começando o projeto de ensino de física apartir de um site

---
+++ app.js
@@ -0,0 +1,6 @@
+const disparar = document.getElementById('Disparar');
+const mensagemCanhao = document.getElementById('mensagemCanhao');
+
+disparar.onclick = function() {
+    mensagemCanhao.innerHTML = "tiro";
+}
